09ff03d7bee40050817981b79c46e143d1c9fcf1
fclinton <foster626@gmail.com>
2020-10-29 01:21:20 -0400

    Update flood source to new active fork

--- post_install.sh
+++ post_install.sh
@@ -18,7 +18,7 @@ sysrc -f /etc/rc.conf rtorrent_flood_enable="YES"
 cd /home/rtorrent || exit 1
 
 # Download sources
-git clone https://github.com/Flood-UI/flood.git
+git clone https://github.com/jesec/flood.git
 cd flood || exit 1
 cp -rf /root/rtorrent/flood/* .
 
